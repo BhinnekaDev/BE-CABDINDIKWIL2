13fd17dc768eda92baabc501c4c984d2de5efaba
fifovalle <fifanaufal10@gmail.com>
2025-10-22 22:16:06 +0700

    feat: intergration with vercel

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,22 @@
-import express from 'express';
 import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
+import express, { Request, Response } from 'express';
 import { ExpressAdapter } from '@nestjs/platform-express';
 
 const server = express();
+let nestAppPromise: Promise<void> | null = null;
 
-const createNestServer = async (expressInstance) => {
-  const app = await NestFactory.create(
-    AppModule,
-    new ExpressAdapter(expressInstance),
-  );
+// Hanya buat server NestJS sekali
+const createNestServer = async () => {
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
   app.enableCors();
   await app.init();
 };
 
-export default async (req, res) => {
-  await createNestServer(server);
+export default async function handler(req: Request, res: Response) {
+  if (!nestAppPromise) {
+    nestAppPromise = createNestServer();
+    await nestAppPromise;
+  }
   server(req, res);
-};
+}
