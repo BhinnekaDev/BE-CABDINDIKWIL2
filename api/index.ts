bca2313ba05fcab4eddb1e450501e04429b3041f
fifovalle <fifanaufal10@gmail.com>
2025-10-22 22:28:33 +0700

    feat: intergration with vercel

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '../src/app.module';
-import express, { Request, Response } from 'express';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import express, { Request, Response } from 'express';
+import { SwaggerModule, DocumentBuilder, OpenAPIObject } from '@nestjs/swagger';
 
 const server = express();
 let nestAppPromise: Promise<void> | null = null;
@@ -11,18 +11,40 @@ const createNestServer = async () => {
   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
   app.enableCors();
 
+  // Swagger / OpenAPI config
   const config = new DocumentBuilder()
     .setTitle('BE-CABDINDIKWIL2 API')
     .setDescription('Backend API Cabang Dinas Pendidikan Wilayah II')
     .setVersion('1.0')
     .build();
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api-docs', app, document);
+  const document: OpenAPIObject = SwaggerModule.createDocument(app, config);
+
+  // Redoc endpoint
+  server.get('/api-docs', (req: Request, res: Response) => {
+    res.send(`
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <title>API Docs</title>
+          <script src="https://cdn.redoc.ly/redoc/latest/bundles/redoc.standalone.js"></script>
+        </head>
+        <body>
+          <redoc spec-url='/api-json'></redoc>
+        </body>
+      </html>
+    `);
+  });
+
+  // Serve OpenAPI JSON
+  server.get('/api-json', (req: Request, res: Response) => {
+    res.json(document);
+  });
 
   await app.init();
 };
 
+// Halaman Selamat Datang
 server.get('/', (req: Request, res: Response) => {
   res.send(`
     <html>
@@ -35,7 +57,7 @@ server.get('/', (req: Request, res: Response) => {
       </head>
       <body>
         <h1>Selamat Datang di BE-CABDINDIKWIL2!</h1>
-        <p>Klik tombol di bawah untuk menuju dokumentasi API (Swagger).</p>
+        <p>Klik tombol di bawah untuk menuju dokumentasi API.</p>
         <button onclick="window.location.href='/api-docs'">Lanjutkan</button>
       </body>
     </html>
